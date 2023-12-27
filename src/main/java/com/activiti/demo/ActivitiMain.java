8685751f4a3db541945946f4c996ac3c20066b90
chuanzhang <2859308825@qq.com>
2023-12-27 17:59:32 +0800

    readme

---
+++ src/main/java/com/activiti/demo/ActivitiMain.java
@@ -0,0 +1,16 @@
+package com.activiti.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 尚智江
+ * @description
+ * @CreateDate 2023/12/26 16:02
+ */
+@SpringBootApplication
+public class ActivitiMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiMain.class, args);
+    }
+}
